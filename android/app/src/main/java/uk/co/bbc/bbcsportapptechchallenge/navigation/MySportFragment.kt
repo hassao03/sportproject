49d54173577538311985a0503baf27ba573400c0
hassao03 <omar.hassan@bbc.co.uk>
2021-09-03 10:50:33 +0100

    Add a new package

--- android/app/src/main/java/uk/co/bbc/bbcsportapptechchallenge/navigation/MySportFragment.kt
+++ android/app/src/main/java/uk/co/bbc/bbcsportapptechchallenge/navigation/MySportFragment.kt
@@ -1,10 +1,11 @@
-package uk.co.bbc.bbcsportapptechchallenge
+package uk.co.bbc.bbcsportapptechchallenge.navigation
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import uk.co.bbc.bbcsportapptechchallenge.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [GuideFragment.newInstance] factory method to
+ * Use the [MySportFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class GuideFragment : Fragment() {
+class MySportFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class GuideFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_guide, container, false)
+        return inflater.inflate(R.layout.fragment_my_sport, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class GuideFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment GuideFragment.
+         * @return A new instance of fragment MySportFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            GuideFragment().apply {
+            MySportFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
