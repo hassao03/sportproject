61292f9c084bca3005d30a9c372df12199463a41
hassao03 <omar.hassan@bbc.co.uk>
2021-09-06 10:43:04 +0100

    Add typography

--- android/app/src/main/java/uk/co/bbc/bbcsportapptechchallenge/ui/SportLayout.kt
+++ android/app/src/main/java/uk/co/bbc/bbcsportapptechchallenge/ui/SportLayout.kt
@@ -1,10 +1,15 @@
 package uk.co.bbc.bbcsportapptechchallenge.ui
 
 import android.content.Context
+import android.content.res.TypedArray
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import android.widget.TextView
+import androidx.annotation.StyleRes
+import androidx.annotation.StyleableRes
+import androidx.core.widget.TextViewCompat
 import com.squareup.picasso.Picasso
 import uk.co.bbc.bbcsportapptechchallenge.R
 import uk.co.bbc.bbcsportapptechchallenge.databinding.FragmentMySportBinding
@@ -27,10 +32,46 @@ class SportLayout @JvmOverloads constructor(
             ViewGroup.LayoutParams.MATCH_PARENT,
             ViewGroup.LayoutParams.WRAP_CONTENT
         )
+
+        setLastUpdatedTextAppearance()
+        setItemTitleTextAppearance()
     }
 
     fun render(model: SportViewModel) {
         presenter.render(model)
+    }
+
+    private fun setItemTitleTextAppearance() {
+        val attrs = context.getStyleAttrs()
+        binding.itemTitle.setTextAppearance(
+            attrs,
+            R.styleable.sportLayout_titleTextAppearance,
+            R.style.titleTextAppearance
+        )
+        attrs.recycle()
+    }
+
+    private fun setLastUpdatedTextAppearance() {
+        val attrs = context.getStyleAttrs()
+        binding.lastUpdatedTimestamp.setTextAppearance(
+            attrs,
+            R.styleable.sportLayout_lastUpdatedTextAppearance,
+            R.style.lastUpdatedTextAppearance
+        )
+        attrs.recycle()
+    }
+
+    private fun TextView.setTextAppearance(attrs: TypedArray, @StyleableRes style: Int, @StyleRes default: Int) {
+        TextViewCompat.setTextAppearance(this, attrs.getResourceId(style, default))
+    }
+
+    private fun Context.getStyleAttrs(): TypedArray {
+        return theme.obtainStyledAttributes(
+            null,
+            R.styleable.sportLayout,
+            R.attr.sportLayoutStyle,
+            R.style.sportLayout
+        )
     }
 
     private class ViewDelegate(val binding: FragmentMySportBinding): SportView {
